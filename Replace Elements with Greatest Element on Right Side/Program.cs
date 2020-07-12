f7c95ffa8988a80dacefee3652474ddd9218314f
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-12 22:04:04 +0200

    Add project files.

---
+++ Replace Elements with Greatest Element on Right Side/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Replace_Elements_with_Greatest_Element_on_Right_Side
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] replaceElements(int[] arr)
+        {
+            int max = -1;
+            for(int i = arr.Length -1; i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = max;
+                max = temp > max ? temp : max;
+            }
+            return arr;
+        }
+    }
+}
